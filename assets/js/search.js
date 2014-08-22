960092a51e11bc38fbcb2d07d3393d682c581ac1
zebheone <zeologism@hotmail.com>
2014-08-22 16:02:42 +0000

    code fixing

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,10 +1,10 @@
         var searchField = $("#search-field").ghostHunter({
                                 results         : "#results",
                                 info_template   : "<p>Pillole trovate: {{amount}}</p>",
-                                result_template : "<a class='btn rss' href='{{link}}'>{{title}}</a>  ",
+                                result_template : "<a href='{{link}}'>{{title}}</a>  ",
                             rss       : "/rss",
                             //Enable the "search as you type" by uncommenting the following line
-                            zeroResultsInfo     : false,
+                            zeroResultsInfo     : true,
                             onKeyUp   : true 
                           });
 
