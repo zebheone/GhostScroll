f6ae14704186e537ac5dd895c21c13edded5c1e1
zebheone <zeologism@hotmail.com>
2014-10-06 13:38:48 +0000

    search phrase mod

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
         var searchField = $("#search-field").ghostHunter({
                                 results         : "#results",
-                                info_template   : "<p>Pillole trovate: {{amount}}</p>",
+                                info_template   : "<p>{{amount}} Post/s found</p>",
                                 result_template : "<a href='{{link}}'>{{title}}</a>  ",
                             rss       : "/rss",
                             //Enable the "search as you type" by uncommenting the following line
