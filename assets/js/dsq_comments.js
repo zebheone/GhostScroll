8bdef230e06657ed05705544d38429d8b62d5a4f
zebheone <zeologism@hotmail.com>
2014-10-15 16:52:18 +0200

    working on better disqus integration

--- assets/js/dsq_comments.js
+++ assets/js/dsq_comments.js
@@ -1,21 +1,9 @@
-var disqus_loaded = false;
-function load_disqus() {
-    if (config-disqus-shortname !== '') {
-        disqus_loaded = true;
-        var disqus_shortname = config-disqus-shortname;
-        // For embedding disqus under a post
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    }
-}
+    var disqus_shortname = 'twelvepurplepills'; // required: replace example with your forum shortname
+    /* * *  var disqus_identifier = '{{post.id}}'; // make sure to use the post.id as an identifier, otherwise disqus will use the pages url per default, which might be problematic... * * */
 
-// Load Disqus only after user has scrolled to bottom of the page
-window.onscroll = function(e) {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        //hit bottom of page
-        if (disqus_loaded==false){ load_disqus() };
-    }
-};
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
