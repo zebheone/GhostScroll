dcb05b35b98f2414ddfcb4d944e47fb7e173ce35
zebheone <zeologism@hotmail.com>
2014-10-15 14:07:00 +0000

    config js for dsq shortname

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+// Should you wish to have google analytics enabled on your site, just add your google analytics key here.
+var google-analytics-key = '';
+
+// Should you wish to have disqus comment system enabled on your site, just add your disqus shortname here.
+var config-disqus-shortname = 'twelvepurplepills';
+
+// Should you wish to have google site verification code on your site, just add your google verification code here.
+var google-site-verification = '';
