9745e682912fcc101fb08c1a1ea3c620958c3a1f
grmmph@gmail.com <grmmph@gmail.com>
2014-11-16 19:57:13 +0100

    Add paralex header like all the cool kids
    Conflicts:
    	assets/js/index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,6 +67,7 @@ var $post = $('.post'),
 					scrollTop: s.offset().top
 				}, 400);
 
+<<<<<<< HEAD
         	})
         })
 
@@ -130,5 +131,69 @@ var $post = $('.post'),
         }
     })
 
+=======
+			});
+		});
+
+		$('.post.last').next('.post-after').hide();
+		if($sitehead.length) { 
+			$(window).scroll( function () {
+				var w = $(window).scrollTop();
+				var g = $sitehead.offset().top;
+				var h = $sitehead.offset().top + $sitehead.height()-100;
+				
+				var paralex = 30 + w/13 + "%";
+				$sitehead.css("background-position-y", paralex);
+
+				if(w >= g && w<=h) {
+					$('.fixed-nav').fadeOut('fast');
+				} else if($(window).width()>500) {
+					$('.fixed-nav').fadeIn('fast');
+				}
+
+				$post.each(function () {
+					var f = $(this).offset().top;
+					var b = $(this).offset().top + $(this).height();
+					var t = $(this).parent('.post-holder').index();
+					var i = $(".fn-item[item_index='"+t+"']");
+					var a = $(this).parent('.post-holder').prev('.post-holder').find('.post-after');
+
+					$(this).attr('item_index', t);
+
+					if(w >= f && w<=b) {
+						i.addClass('active');
+						a.fadeOut('slow');
+					} else {
+						i.removeClass('active');
+						a.fadeIn('slow');
+					}
+				});
+			});
+		}
+
+		$('ul li').before('<span class="bult fa fa-asterisk icon-asterisk"></span>');
+		$('blockquote p').prepend('<span class="quo icon-quote-left"></span>');
+		$('blockquote p').append('<span class="quo icon-quote-right"></span>');
+	});
+	
+	$post.each(function () {
+		var postText = $(this).html();
+		var fa  = [];
+		for(var i=0; i < icons.length; i++) {
+			fa[i]       = {};
+			fa[i].str   = "@"+ icons[i]+ "@";
+			fa[i].icon  = icons[i];
+			fa[i].int   = postText.search(fa[i].str);
+
+			if(fa[i].int > -1 ) { 
+				fa[i].count = postText.match(new RegExp(fa[i].str,"g")).length;
+				for(var j=0; j < fa[i].count; j++) {
+					$(this).html($(this).html().replace(fa[i].str, "<i class='fa "+fa[i].icon+"'></i>"))
+				}
+			}
+		}
+	});
+	
+>>>>>>> 7321ed9... Add paralex header like all the cool kids
 
 }(jQuery));
