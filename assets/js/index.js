b15006ed9e1789f85ecb8e36cdb0a0848eccbc32
grmmph@gmail.com <grmmph@gmail.com>
2014-11-16 19:57:13 +0100

    Add paralex header like all the cool kids
    Conflicts:
    	assets/js/index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -71,7 +71,10 @@ var $sitehead = $('#site-head');
 			$(window).scroll( function () {
 				var w = $(window).scrollTop();
 				var g = $sitehead.offset().top;
-				var h = $sitehead.offset().top + $(this).height()-100;
+				var h = $sitehead.offset().top + $sitehead.height()-100;
+				
+				var paralex = 30 + w/13 + "%";
+				$sitehead.css("background-position-y", paralex);
 
 				if(w >= g && w<=h) {
 					$('.fixed-nav').fadeOut('fast');
